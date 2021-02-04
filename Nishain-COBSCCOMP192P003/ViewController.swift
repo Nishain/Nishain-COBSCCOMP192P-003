e48b9b7c550b4cfd42443fcbc18654da5c4afe7b
Nishain <nish>
2021-02-03 22:03:55 -0800

    Initial Commit

---
+++ Nishain-COBSCCOMP192P003/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nishain-COBSCCOMP192P003
+//
+//  Created by Nishain on 2/3/21.
+//  Copyright © 2021 Nishain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nishain-COBSCCOMP192P003Tests/Nishain_COBSCCOMP192P003Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Nishain_COBSCCOMP192P003Tests.swift
+//  Nishain-COBSCCOMP192P003Tests
+//
+//  Created by Nishain on 2/3/21.
+//  Copyright © 2021 Nishain. All rights reserved.
+//
+
+import XCTest
+@testable import Nishain_COBSCCOMP192P003
+
+class Nishain_COBSCCOMP192P003Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
